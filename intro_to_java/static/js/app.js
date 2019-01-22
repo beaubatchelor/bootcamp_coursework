49fd5bda918cb54094ddb9f9fb445acfe5efccd8
BeauWB <beauwbatchelor@gmail.com>
2019-01-22 15:51:48 -0800

    adding all code

--- intro_to_java/static/js/app.js
+++ intro_to_java/static/js/app.js
@@ -1,4 +1,48 @@
-// from data.js
-var tableData = data;
+// datetime: "1/1/2010",
+// city: "benton",
+// state: "ar",
+// country: "us",
+// shape: "circle",
+// durationMinutes: "5 mins.",
+// comments: "4 bright green"
 
-// YOUR CODE HERE!
+
+// from data.js
+const tableData = data;
+
+// from index.html
+var tbody = d3.select('tbody')
+
+var field = d3.select('#datetime')
+
+var button = d3.select('#filter-btn');
+
+// clear old data
+tbody.html('')
+
+tableData.forEach(function(data){
+    Object.entries(data).forEach(function([key, value]){
+        tbody.append('td').text(value);
+    });
+});
+
+// button function
+function buttonClick(){
+    button.on('click', function(e){
+        let fieldText = document.querySelector('#datetime').value;
+        alert(fieldText);
+        tbody.html('')
+        tableData.forEach(function(data){
+            tbody.append('tr');
+            Object.entries(data).forEach(function([key, value]){
+                if (value === fieldText)
+                tbody.append('td').text(value);
+            });
+        });
+    })
+};
+
+
+
+
+buttonClick()
