8f83685d963b14f666c4d7496f861ab0be68d0e7
Beau Batchelor <beauwbatchelor@gmail.com>
2019-01-23 22:15:36 -0800

    adding final code

--- intro_to_java/static/js/app.js
+++ intro_to_java/static/js/app.js
@@ -1,48 +1,37 @@
-// datetime: "1/1/2010",
-// city: "benton",
-// state: "ar",
-// country: "us",
-// shape: "circle",
-// durationMinutes: "5 mins.",
-// comments: "4 bright green"
-
-
 // from data.js
 const tableData = data;
 
 // from index.html
 var tbody = d3.select('tbody')
 
-var field = d3.select('#datetime')
-
 var button = d3.select('#filter-btn');
 
-// clear old data
-tbody.html('')
 
-tableData.forEach(function(data){
-    Object.entries(data).forEach(function([key, value]){
-        tbody.append('td').text(value);
-    });
-});
-
-// button function
-function buttonClick(){
-    button.on('click', function(e){
-        let fieldText = document.querySelector('#datetime').value;
-        alert(fieldText);
-        tbody.html('')
-        tableData.forEach(function(data){
-            tbody.append('tr');
-            Object.entries(data).forEach(function([key, value]){
-                if (value === fieldText)
-                tbody.append('td').text(value);
-            });
-        });
-    })
+function dateFilter(date){
+    return date >= fieldText
 };
 
+function makeData(data){
+    tbody.html('');
+    data.forEach(function(entry){
+        tbody.append("tr");
+        Object.entries(entry).forEach(function([key, value]){
+            tbody.append('td').text(value);
+        });
+    });
+};
 
+makeData(tableData);
 
+function buttonClicks() {
+    let fieldText = d3.select('#datetime').property("value")
+    let newData = tableData;
+    if (fieldText) {
+        newData = newData.filter(row => {
+            return row.datetime === fieldText;
+        });
+    };
+    makeData(newData)
+};
 
-buttonClick()
+button.on('click', buttonClicks);
